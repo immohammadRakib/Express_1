9d80dfc67bc37e9e8d9325e10716e2ba9115fb95
Md Abdur Rahman Rakib <rakiblp6@gmail.com>
2026-05-16 17:31:47 +0600

    polish codebase

--- src/app.ts
+++ src/app.ts
@@ -20,25 +20,6 @@ app.get("/", async(req: Request, res: Response) => {
     return res.status(200).json({ message: "Hello, World!" });
 });
 
-// api to create a new user
-
-
-// api to get all users
-
-
-
-// api to get a user by id
-
-
-
-
-// api to update a user by id
- 
-
-// api to delete a user by id
-
-
-
 
 
 
