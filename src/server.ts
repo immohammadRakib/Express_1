3727b77f5d9ad2f874459acfdfe0e5501f0921ca
Md Abdur Rahman Rakib <rakiblp6@gmail.com>
2026-06-02 14:28:25 +0600

    redis-added

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,21 @@
+import { createClient } from "redis";
 import app from "./app";
 import config from "./config";
 import { initDB } from "./db";
 
 
+export const redisClient =createClient({
+   url: "redis://redis-cache:6379"
+})
+
+redisClient.on("error", (err) => console.error("Redis Client Error:", err));
+redisClient.on("connect", () => console.log("Redis Connected Successfully! ⚡"));
+
+
+
 const main = async() => {
   await initDB();
+  await redisClient.connect();
 
   app.listen(config.port, () => {
   console.log(`server is running on port ${config.port}`);
